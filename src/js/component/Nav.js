c35f010b6e7bcf267e88b119694c11b5f22bd33a
josepadron370 <josepadron370@gmail.com>
2019-07-22 15:56:08 +0000

    Working with Cards

--- src/js/component/Nav.js
+++ src/js/component/Nav.js
@@ -5,7 +5,32 @@ export class Nav extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>NAV!</h1>
+				<ul className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top nav nav-pills nav-fill">
+					<li className="nav-item">
+						<a className="nav-link active" href="#">
+							Active
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Much longer nav link
+						</a>
+					</li>
+					<li className="nav-item">
+						<a className="nav-link" href="#">
+							Link
+						</a>
+					</li>
+					<li className="nav-item">
+						<a
+							className="nav-link disabled"
+							href="#"
+							tabIndex="-1"
+							aria-disabled="true">
+							Disabled
+						</a>
+					</li>
+				</ul>
 			</div>
 		);
 	}
